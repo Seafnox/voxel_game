304c564ad3c8290850fabb0d34fe8d07cd3a4256
Kirill Warp <kirill9990@gmail.com>
2023-07-31 02:14:55 +0300

    add copy webpack plugin for assets

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV === 'production';
@@ -37,7 +38,11 @@ const config = {
       title: 'Miniverse',
       template: './src/index.html',
     }),
-
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: './src/resources', to: 'resources' }
+      ]
+    })
     // Add your plugins here
     // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],
