94c0c1afaddb3c34b347376a4308c6132e5b088a
Kirill Warp <kirill9990@gmail.com>
2024-05-19 02:48:00 +0300

    refactor: remove old Fixme

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import { PseudoRandomizer } from './utils/PseudoRandomizer';
 import { VMath } from './VMath';
 import { VoxelGame } from './VoxelGame';
 
-// FIXME import randomizer into engine and refactor Math.random
 const randomizer = new PseudoRandomizer(1152372536);
 VMath.random = randomizer.next.bind(randomizer);
 
